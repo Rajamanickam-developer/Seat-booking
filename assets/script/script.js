5fb50d1a6de92dab9e409b8f917bed5a9e75fc58
Rajamanickam <rajamanickam842001@gmail.com>
2023-03-27 10:03:31 +0530

    completed design

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const movie = document.getElementById('options');
+// console.log(movie);
+
+const seats = document.querySelectorAll('.row .seat');
+// console.log(seats);
+
+const ticketCount = document.getElementById('count');
+// console.log(ticketCount);
+
+const totalPrice = document.getElementById('price');
+// console.log(totalPrice);
+
